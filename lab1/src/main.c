72b23fcc4bd89c252d91e33530fa774debbaa54c
bychkovarthur <bychkovarthur@gmail.com>
2023-09-20 11:23:57 +0300

    delete funcs from main

--- lab1/src/main.c
+++ lab1/src/main.c
@@ -1,48 +1,7 @@
 #include "stdlib.h"
 #include <unistd.h>
 #include <stdio.h>
-
-int getNumberOfNumbers(char* stringOfNumbers, int stringLen) {
-    if (stringLen == 0 || (stringLen == 1 && stringOfNumbers[0] == '\n')) {
-        return 0;
-    }
-    int spaceCount = 0;
-    for (int i = 0; i < stringLen; ++i) {
-        if (stringOfNumbers[i] == ' ') {
-            ++spaceCount;
-        }
-    }
-    return spaceCount + 1;
-}
-
-void fillArrayWithNumbers(char *stringOfNumbers, int stringLen, int* array) {
-    int currentNumber = 0;
-    int arrayIndex = 0;
-    for (int i = 0; i < stringLen; ++i) {
-        if (!(stringOfNumbers[i] == ' ' || stringOfNumbers[i] == '\n')) {
-            currentNumber *= 10;
-            currentNumber += stringOfNumbers[i] - '0';
-        } else {
-            array[arrayIndex] = currentNumber;
-            ++arrayIndex;
-            currentNumber = 0;
-        }
-    }
-    // Если конец строки через EOF, нужно обработать последний элемент
-    if (currentNumber) {
-        array[arrayIndex] = currentNumber;
-    }
-}
-
-double devide(int* array, int numberOfNumbers) {
-    double result = array[0];
-    
-    for (int i = 1; i < numberOfNumbers; ++i) {
-        result /= array[i];
-    }
-    
-    return result;
-}
+#include "../include/functions.h"
 
 
 int main(int argc, char **argv)
