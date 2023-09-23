d126e596983b9734f32e84356c1eb845235f410a
bychkovarthur <bychkovarthur@gmail.com>
2023-09-23 23:14:21 +0300

    lab complete

--- lab1/include/functions.h
+++ lab1/include/functions.h
@@ -9,4 +9,6 @@ double devide(int* array, int numberOfNumbers, int* childProcessExitStatus);
 
 void fileNameValidation(char* fileName);
 
+void updateQuitStatus(char* stringOfNumbers, int charactersReaded, int* qStatus);
+
 #endif //LAB1_FUNCTIONS_H
--- lab1/src/parent.c
+++ lab1/src/parent.c
@@ -7,11 +7,10 @@
 #include <signal.h>
 #include <linux/limits.h>
 
-// Нужно, чтобы пользователь мог воводить много команд
 
 int main(int argc, char **argv)
 {
-    // Чтение имени файла
+    // FileName
     ssize_t fileNameLen = 128;
     char* fileName = (char  *) malloc(fileNameLen * sizeof(char));
     printf("Введите название файла:\n");
@@ -20,9 +19,9 @@ int main(int argc, char **argv)
         perror("FileName error (parent)");
         exit(1);
     }
-
     fileNameValidation(fileName);
 
+    //Pipe's
     int pipe1[2];
     int pipe2[2];
     if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
@@ -30,50 +29,27 @@ int main(int argc, char **argv)
         exit(1);
     }
 
+    // Fork
     pid_t pid = fork();
     if (pid == -1) {
         perror("fork");
         exit(1);
     }
 
-    if (pid > 0) { // parent procces
+    if (pid > 0) { // Parent procces
+        
+        int qStatus = 0;
+        char *inputString;
+        size_t n = 1024;
+        ssize_t charactersReaded;
+        int childProcessExitStatus;
+
         if (close(pipe1[0]) == -1 || close(pipe2[1]) == -1) {
             perror("Can't close file descriptor (parent)");
             exit(1);
         }
 
-        printf("Введите числа:\n");
-
-        char *inputString;
-        size_t n = 1024;
-        ssize_t charactersReaded;
-        inputString = (char *) malloc(n * sizeof(char));
-
-        if (inputString == NULL) {
-            perror("Unable allocate buffer (parent)");
-            exit(1);
-        }
-
-        if ((charactersReaded = getline(&inputString, &n, stdin)) == -1) { // Считывается перенос тоже
-            perror("GetLine error (parent)");
-            kill(pid, SIGKILL); // Если ничего не прочитали в родительском, убиваем дочерний
-            exit(1);
-        }
-
-        // Получение количества чисел в строке
-        int numberOfNumbers = getNumberOfNumbers(inputString, charactersReaded);
-        if (numberOfNumbers <= 0) {
-            perror("Invalid inputString (parent)");
-            exit(1);
-        }
-
-        // Получение массива с числами
-        int *arrayOfNumbers = malloc(sizeof(int) * numberOfNumbers);
-        if (arrayOfNumbers == NULL) {
-            perror("Unable allocate buffer (parent)");
-            exit(1);
-        }
-        fillArrayWithNumbers(inputString, charactersReaded, arrayOfNumbers);
+        printf("Введите числа или q для выхода:\n");
 
         // Перенаправляем STDOUT на PIPE1
         if (dup2(pipe1[1], STDOUT_FILENO) == -1) {
@@ -81,26 +57,72 @@ int main(int argc, char **argv)
             exit(1);    
         }
 
-        // Перенаправляем STDIN на PIPE2
-        if (dup2(pipe2[0], STDIN_FILENO) == -1) {
-            perror("dup2 stdin error (parent)");
-            exit(1);
-        }
+        while (1) {
+            inputString = (char *) malloc(n * sizeof(char));
 
-        // Передаем размер массива и сам массив
-        if (write(STDOUT_FILENO, &numberOfNumbers, sizeof(int)) != sizeof(int) || write(STDOUT_FILENO, arrayOfNumbers, sizeof(int) * numberOfNumbers) != sizeof(int) * numberOfNumbers) {
-            perror("Can't write (parent)");
-            exit(1);
+            if (inputString == NULL) {
+                perror("Unable allocate buffer (parent)");
+                exit(1);
+            }
+
+            if ((charactersReaded = getline(&inputString, &n, stdin)) == -1) { // Считываем входную строку с переносом
+                perror("GetLine error (parent)");
+                exit(1);
+            }
+
+            updateQuitStatus(inputString, charactersReaded, &qStatus);
+            if(write(STDOUT_FILENO, &qStatus, sizeof(int)) != sizeof(int)) {
+                perror("Can't write qStatus (parent)");
+            }
+
+            if (qStatus) {
+                free(inputString);
+                break;
+            }
+            
+            // Получение количества чисел в строке
+            int numberOfNumbers = getNumberOfNumbers(inputString, charactersReaded);
+            if (numberOfNumbers <= 0) {
+                perror("Invalid inputString (parent)");
+                exit(1);
+            }
+
+            // Получение массива с числами
+            int *arrayOfNumbers = malloc(sizeof(int) * numberOfNumbers);
+            if (arrayOfNumbers == NULL) {
+                perror("Unable allocate buffer (parent)");
+                exit(1);
+            }
+            fillArrayWithNumbers(inputString, charactersReaded, arrayOfNumbers);
+            free(inputString);
+            
+            // Передаем размер массива и сам массив
+            if (write(STDOUT_FILENO, &numberOfNumbers, sizeof(int)) != sizeof(int) || write(STDOUT_FILENO, arrayOfNumbers, sizeof(int) * numberOfNumbers) != sizeof(int) * numberOfNumbers) {
+                perror("Can't write (parent)");
+                exit(1);
+            }
+
+            free(arrayOfNumbers);
+
+            // Проверка статуса дочернего процесса
+            if (read(pipe2[0], &childProcessExitStatus, sizeof(int)) != sizeof(int)) {
+                perror("Can't read data from child (parent)");
+                exit(1);
+            }
+
+            if (childProcessExitStatus) {
+                perror("Child process error (parent)");
+                exit(1);
+            }
         }
 
         if (waitpid(pid, NULL, 0) == -1) {
             perror("WaitPID error (parent)");
+            exit(1);
         }
-        free(inputString);
 
         // Проверка статуса дочернего процесса
-        int childProcessExitStatus;
-        if (read(STDIN_FILENO, &childProcessExitStatus, sizeof(int)) == -1) {
+        if (read(pipe2[0], &childProcessExitStatus, sizeof(int)) != sizeof(int)) {
             perror("Can't read data from child (parent)");
             exit(1);
         }
@@ -117,7 +139,7 @@ int main(int argc, char **argv)
 
     }
 
-    if (pid == 0) { // child procces
+    if (pid == 0) { // Child procces
 
         if (close(pipe1[1]) == -1 || close(pipe2[0]) == -1) {
             perror("Can't close file descriptor (parent)");
