3a0dd69f760138ac3fa99b86defcb30906054e85
bychkovarthur <bychkovarthur@gmail.com>
2023-09-30 16:52:15 +0300

    add mergeSort.h

---
+++ lab2/include/mergeSort.h
@@ -0,0 +1,9 @@
+#ifndef MERGE_SORT_H
+#define MERGE_SORT_H
+
+int* merge(int*, int*, int*, const unsigned int, const unsigned int);
+int* mergeSortAlgorithm(int*, int*, unsigned int, int, int);
+int* mergeSort(int*, int*, int);
+
+
+#endif // MERGE_SORT_H
