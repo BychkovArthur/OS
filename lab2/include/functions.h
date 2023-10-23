6f50376064c6315704b40306bdac71bb09018b58
bychkovarthur <bychkovarthur@gmail.com>
2023-10-23 19:21:01 +0300

    update headers

--- lab2/include/functions.h
+++ lab2/include/functions.h
@@ -1,8 +1,14 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include "structures.h"
+
 int isArrayStrictlyIncreasing(int*, int);
+int isArraysStrictlyIncreasing(int**, int, int);
 void usage();
 int strToInt(char*);
+void createRandomTasks(int**, int**, Task*, int, int, int, int*);
+void executeTask(Task*, int);
+void finishTask(int*, int*, Task*, int);
 
 #endif // FUNCTIONS_H
--- lab2/include/structures.h
+++ lab2/include/structures.h
@@ -1,11 +1,23 @@
 #ifndef STRUCTURES_H
 #define STRUCTURES_H
 
-typedef struct {
+typedef struct threadData_t threadData_t;
+
+typedef struct Task Task;
+
+typedef struct threadData_t {
     int* arr;
     int* buff;
     int size;
     int threadCnt;
 } threadData_t;
+
+typedef struct Task {
+    int* array;
+    int* buffer;
+    int size;
+    int type;
+    Task* otherTask;
+} Task;
 
 #endif // STRUCTURES_H
