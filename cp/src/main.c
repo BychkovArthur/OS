d0c2e8554afe01eba1abbb3f8b5202adfe1b956f
Arthur Bychkov <bychkovarthur@gmail.com>
2023-12-10 22:53:32 +0300

    can allocating memory, but always last must be blockInfo

--- cp/src/main.c
+++ cp/src/main.c
@@ -2,6 +2,7 @@
 #include "../include/blockInfo.h"
 
 #include <stdio.h>
+#include <stdint.h>
 
 void printBinary(size_t num) {
     int bits = sizeof(size_t) * 8; // Количество бит в int
@@ -16,11 +17,23 @@ int main() {
     size_t d;
     scanf("%zu", &d);
     Allocator* allocator = createMemoryAllocator(d);
-
-    printf("%p\n", ((BlockInfo*)allocator->memory)->nextBlock);
+    BlockInfo* firstBlock = ((BlockInfo*)allocator->memory);
+    printf("%p\n", (void*)firstBlock->nextBlock);
     printf("HERE 0 %p: \n", allocator->memory);
-    printf("HERE %p\n", allocator->firstBlock);
-    printf("%zu", getBlockLength(allocator, (BlockInfo*)allocator->memory));
+    printf("HERE %p\n", (void*)firstBlock);
+    printf("%zu\n", getBlockLengthByBlock(allocator, (BlockInfo*)allocator->memory));
+
+    void* alloc1 = allocBlock(allocator, 10);
+    
+    void* alloc2 = allocBlock(allocator, 30);
+    void* alloc3 = allocBlock(allocator, 16);
+    printf("Начало памяти       : %ld\nНачало первого блока: %ld\nНачало второго блока: %ld\n", allocator->memory, alloc1 - allocator->memory, alloc2 - allocator->memory);
+    printf("Размер первого блока: %zu\n", getBlockLengthByGivenMemory(allocator, alloc1));
+    printf("Размер второго блока: %zu\n", getBlockLengthByGivenMemory(allocator, alloc2));
+    printf("Размер третьего блока: %zu\n", getBlockLengthByBlock(allocator, (BlockInfo*)(alloc2 + 32)));
+    printf("Размер третьего блока: %zu\n", getBlockLengthByGivenMemory(allocator, (void*)((uint8_t*)alloc2 + 40)));
+    printf("HERE %ld\n", (alloc2 + 32) - allocator->memory);
+    printf("ptr: %p\n", ((BlockInfo*)(alloc2 + 32))->nextBlock);
     // BlockInfo block = {
     //     NULL,
     // };
--- cp/src/resourceMapAllocator.c
+++ cp/src/resourceMapAllocator.c
@@ -24,7 +24,7 @@ Allocator* createMemoryAllocator(size_t memorySize) {
     // Добавляем размер структуры
     memorySize += ALIGN_BY;
     memorySize = align(memorySize); // Выравниваем саму память
-
+    printf("MEMORY SIZE: %zu\n", memorySize);
     uint8_t* memory = malloc(memorySize); // malloc выравнивает память по 8 (или 16 или 32...)
     if (memory == NULL) {
         fprintf(stderr, "Can't allocate memory\n");
@@ -38,53 +38,53 @@ Allocator* createMemoryAllocator(size_t memorySize) {
     BlockInfo block = {
         NULL,
     };
-    *(BlockInfo*)allocator->memory = block;
+    BlockInfo* firstBlock = (BlockInfo*)allocator->memory;
+    *firstBlock = block;
 
-    allocator->firstBlock = setBlockFree((BlockInfo*)allocator->memory);
+    firstBlock->nextBlock = setBlockFree(firstBlock->nextBlock);
     return allocator;
 }
 
-size_t getBlockLength(Allocator* allocator, BlockInfo* blockInfo) {
-    if (blockInfo->nextBlock == NULL) {
+size_t getBlockLengthByBlock(Allocator* allocator, BlockInfo* blockInfo) {
+    BlockInfo* nextBlock = resetToNormalPointer(blockInfo->nextBlock);
+    if (nextBlock == NULL) {
+        printf("%ld\n\n\n", blockInfo->nextBlock);
         return allocator->memorySize - ((uint8_t*)blockInfo - (uint8_t*)allocator->memory) - ALIGN_BY;
     }
+    return ((uint8_t*)nextBlock - (uint8_t*)blockInfo) - ALIGN_BY;
+}
 
-    return ((uint8_t*)resetToNormalPointer(blockInfo->nextBlock) - (uint8_t*)blockInfo) - ALIGN_BY;
+size_t getBlockLengthByGivenMemory(Allocator* allocator, void* memory) {
+    return getBlockLengthByBlock(allocator, (BlockInfo*)((uint8_t*)memory - ALIGN_BY));
 }
 
 void* allocBlock(Allocator* allocator, size_t blockSize) {
 
     blockSize += ALIGN_BY;
     blockSize = align(blockSize);
-    BlockInfo* currentBlock = resetToNormalPointer(allocator->firstBlock);
-
-    // Если всего один блок
-    if (currentBlock->nextBlock == NULL) {
-        allocator->firstBlock = setBlockOccupied(currentBlock);
-        currentBlock->nextBlock = (BlockInfo*)((uint8_t*)currentBlock + blockSize);
-        currentBlock->nextBlock->nextBlock = NULL;
-        currentBlock->nextBlock = setBlockFree(currentBlock->nextBlock);
-        return (void*)((uint8_t)currentBlock->nextBlock + ALIGN_BY);
-    }
-
+    BlockInfo* currentBlock = (BlockInfo*)allocator->memory;
     BlockInfo* bestBlock = NULL;
     size_t minLength = -1;
 
     while (currentBlock != NULL) {
-        size_t currentBlockLength = getBlockLength(allocator, currentBlock);
-        if (currentBlock->isFree && currentBlockLength >= blockSize && currentBlockLength < minLength) {
-            bestBlock = resetToNormalPointer(currentBlock);
+        size_t currentBlockLength = getBlockLengthByBlock(allocator, currentBlock);
+        if (isBlockFree(currentBlock->nextBlock) && currentBlockLength >= blockSize && currentBlockLength < minLength) {
+            bestBlock = currentBlock;
             minLength = currentBlockLength;
         }
         currentBlock = resetToNormalPointer(currentBlock->nextBlock);
     }
 
+    if (bestBlock == NULL) {
+        fprintf(stderr, "Can't allocate new memory\n");
+        exit(1);
+    }
+
     BlockInfo* newBlock = (BlockInfo*)((uint8_t*)bestBlock + blockSize);
-    newBlock->nextBlock = bestBlock->nextBlock;
-    newBlock->isFree = true;
-    bestBlock->nextBlock = newBlock;
-    bestBlock->isFree = false;
-    return ((uint8_t*)bestBlock) + ALIGN_BY;
+    newBlock->nextBlock = resetToNormalPointer(bestBlock->nextBlock);
+    newBlock->nextBlock = setBlockFree(newBlock->nextBlock);
+    bestBlock->nextBlock = setBlockOccupied(newBlock);
+    return (void*)(((uint8_t*)bestBlock) + ALIGN_BY);
 }
 
 void* freeBlock(Allocator* allocator, void* block);
